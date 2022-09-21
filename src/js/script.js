79efe4daeb4eed70ec1d90f251d5995b6a972b56
AbremkoA <abremko@gmail.com>
2022-09-21 18:24:16 +0200

    add modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -36,4 +36,20 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
-  });
+    // Modal 
+
+    $('[data-modal=consultation]').on('click', function(){
+		$('.overlay, #consultation').fadeIn();
+    });
+
+	$('.modal__close, .overlay').on('click', function() {
+		$('.overlay, #consultation, #order, #thanks').fadeOut();
+	});
+
+	$('.button_mini').each(function(i) {
+		$(this).on('click', function() {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn();
+		});
+	});
+});
